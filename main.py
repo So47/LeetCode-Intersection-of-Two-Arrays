0f3428d0c98a855610cee49ac9bc5892101d0034
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-03-13 09:40:53 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+
+
+        return nums1_set.intersection(nums2_set)
+        # intersection = []
+
+        # for num in nums1_set:
+        #     if num in nums2_set:
+        #         intersection.append(num)
+        # return intersection     
+
+
+        
